1c6217153e2baae811bea842b94432f1664b5834
bapanroy <bapanroy280@gmail.com>
2024-09-26 14:00:10 +0530

    update

--- routes/web.php
+++ routes/web.php
@@ -29,43 +29,7 @@ Route::group([
         return redirect()->route('login');
     });
 });
-// Route::group([
-//     "middleware" => ["auth"]
-// ], function () {
-//     //dashboard
-//     Route::get("dashboard", [AuthCountroller::class, "dashboard"])->name("dashboard");
 
-//     //profile
-//     Route::match(["get", "post"], "profile", [AuthCountroller::class, "profile"])->name("profile");
-//     //register
-//     Route::get("logout", [AuthCountroller::class, "logout"])->name("logout");
-//     //item-groups
-//     Route::get('/item-groups', [ItemGroupController::class, 'index'])->name('item-groups.index');
-//     Route::get('/item-groups/create', [ItemGroupController::class, 'create'])->name('item-groups.create');
-//     Route::post('/item-groups/store', [ItemGroupController::class, 'store'])->name('item-groups.store');
-//     Route::get('/item-groups/show/{id}', [ItemGroupController::class, 'show'])->name('item-groups.show');
-//     Route::get('/item-groups/{id}/edit', [ItemGroupController::class, 'edit'])->name('item-groups.edit');
-//     Route::put('/item-groups/update/{id}', [ItemGroupController::class, 'update'])->name('item-groups.update');
-//     Route::delete('/item-groups/{id}', [ItemGroupController::class, 'destroy'])->name('item-groups.destroy');
-//     //item
-//     Route::get('/items', [ItemController::class, 'index'])->name('items.index');
-//     Route::get('/items/create', [ItemController::class, 'create'])->name('items.create');
-//     Route::post('/items/store', [ItemController::class, 'store'])->name('items.store');
-//     Route::get('/items/show/{id}', [ItemController::class, 'show'])->name('items.show');
-//     Route::get('/items/{id}/edit', [ItemController::class, 'edit'])->name('items.edit');
-//     Route::put('/items/update/{id}', [ItemController::class, 'update'])->name('items.update');
-//     Route::delete('/items/{id}', [ItemController::class, 'destroy'])->name('items.destroy');
-
-
-//     //expenditures
-//     Route::get('/expenditures', [ExpenditureController::class, 'index'])->name('expenditures.index');
-//     Route::get('/expenditures/create', [ExpenditureController::class, 'create'])->name('expenditures.create');
-//     Route::post('/expenditures/store', [ExpenditureController::class, 'store'])->name('expenditures.store');
-//     Route::get('/expenditures/show/{id}', [ExpenditureController::class, 'show'])->name('expenditures.show');
-//     Route::get('/expenditures/{id}/edit', [ExpenditureController::class, 'edit'])->name('expenditures.edit');
-//     Route::put('/expenditures/update/{id}', [ExpenditureController::class, 'update'])->name('expenditures.update');
-//     Route::delete('/expenditures/{id}', [ExpenditureController::class, 'destroy'])->name('expenditures.destroy');
-// });
 Route::group([
     "middleware" => ["auth"]
 ], function () {
